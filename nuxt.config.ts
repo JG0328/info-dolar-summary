c679189adbaaeb2153c9d550df8a0b38e8f20604
José Gabriel González <jgg032899@gmail.com>
2025-11-06 22:49:23 -0400

    Changed website name
    - Added favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,18 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss']
+  modules: ['@nuxtjs/tailwindcss'],
+  app: {
+    head: {
+      title: 'Dólar Hoy RD',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        {
+          name: 'description',
+          content: 'Consulta los precios del dólar hoy en República Dominicana. Información actualizada de casas de cambio y bancos.'
+        }
+      ]
+    }
+  }
 })
